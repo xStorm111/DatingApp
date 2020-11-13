fb56b8f844ce930ab46c2b007c26f7ff2e72a12f
xStorm111 <joaquimrodriguesdx@hotmail.com>
2020-11-13 12:19:53 +0000

    End of Section 2

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,42 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        //IEnumerable is for list/arrays, not single objects
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = _context.Users.ToListAsync();
+
+            return await users;
+        }
+
+        // api/users/3
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+
+            //in the course Niel says we can reduce this to only return _context.Users.Find(id);
+            //i still prefer this way because its a lot easier to debug if we need too
+            var user = _context.Users.FindAsync(id);
+            return await user;
+        }
+    }
+}
