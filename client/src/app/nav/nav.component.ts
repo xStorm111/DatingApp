90328007d47e86049ccc76fbf66bc42a61895982
xStorm111 <joaquimrodriguesdx@hotmail.com>
2021-02-08 13:55:43 +0000

    remove login console.log

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -25,7 +25,7 @@ export class NavComponent implements OnInit {
   }
 
   login() {
-    console.log(this.model)
+    // console.log(this.model)
     this.accountService.login(this.model).subscribe(response => {
       this.router.navigateByUrl('/members')
     })
